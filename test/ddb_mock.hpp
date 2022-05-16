b4b3c90e0e9294ef8993e565babbad0a7cf1d8f6
Robin Ekman <robin.seth.ekman@gmail.com>
2022-05-16 15:55:08 +0200

    remove superfluous test files

--- test/ddb_mock.hpp
+++
@@ -1,9 +0,0 @@
-#include <deadbeef/deadbeef.h>
-#include "../src/defs.hpp"
-
-namespace ddb_ipc{
-
-DB_functions_t get_ddb_mock_api();
-void conf_get_str(const char* key, const char* def, char* buffer, int buffer_size);
-
-}
